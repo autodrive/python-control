b54d7f9954639f01dfea21162dbc8e224d3063cd
KangWon LEE <kangwon_fwd@naver.com>
2016-06-16 02:01:49 +0900

    find_slycot.py : build list of lines with slycot

--- slycot_conversion/find_slycot.py
+++ slycot_conversion/find_slycot.py
@@ -1,12 +1,14 @@
 import os
 import re
+from pprint import pprint
 
 
 def pwd():
     return os.path.abspath(os.curdir)
 
 
-def process_file(file_name):
+def process_file(path, file_name):
+    result = []
     if ".py" == os.path.splitext(file_name)[-1]:
         with open(file_name, 'r') as f:
             txt = f.read()
@@ -16,23 +18,33 @@ def process_file(file_name):
 
             for k, line in enumerate(lines):
                 if "slycot" in line:
-                    print file_name, k, ':', line
+                    # print file_name, k, ':', line
+                    result.append((path, file_name, k, line))
+    return result
 
 
 def process_folder(root, files):
     current_path = pwd()
     os.chdir(os.path.abspath(root))
+
+    folder_result = []
+
     for file_name in files:
-        process_file(file_name)
+        folder_result.append(process_file(root, file_name))
 
     os.chdir(current_path)
+
+    return folder_result
 
 
 def main():
     current_path = pwd()
 
+    result = []
     for root, dirs, files in os.walk(os.pardir):
-        process_folder(root, files)
+        result.append(process_folder(root, files))
+
+    pprint(result)
 
     os.chdir(current_path)
 
