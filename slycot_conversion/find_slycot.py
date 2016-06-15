a19b2d7832560aa5535965ccbcaed621bbca3966
KangWon LEE <kangwon_fwd@naver.com>
2016-06-16 01:56:55 +0900

    find_slycot.py : print all lines including slycot

--- slycot_conversion/find_slycot.py
+++ slycot_conversion/find_slycot.py
@@ -1,8 +1,38 @@
 import os
+import re
+
+
+def pwd():
+    return os.path.abspath(os.curdir)
+
+
+def process_file(file_name):
+    if ".py" == os.path.splitext(file_name)[-1]:
+        with open(file_name, 'r') as f:
+            txt = f.read()
+            f.close()
+
+            lines = txt.splitlines()
+
+            for k, line in enumerate(lines):
+                if "slycot" in line:
+                    print file_name, k, ':', line
+
+
+def process_folder(root, files):
+    current_path = pwd()
+    os.chdir(os.path.abspath(root))
+    for file_name in files:
+        process_file(file_name)
+
+    os.chdir(current_path)
 
 
 def main():
-    current_path = os.path.abspath(os.curdir)
+    current_path = pwd()
+
+    for root, dirs, files in os.walk(os.pardir):
+        process_folder(root, files)
 
     os.chdir(current_path)
 
