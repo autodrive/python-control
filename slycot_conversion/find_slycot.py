73b38eb4bd58f239546cfc3eae9e8f5d3d96d4ed
KangWon LEE <kangwon@gmail.com>
2016-06-16 14:22:53 +0900

    find_slycot.py : removed process_folder() -> RecursiveFinder.process_folder()

--- slycot_conversion/find_slycot.py
+++ slycot_conversion/find_slycot.py
@@ -23,22 +23,6 @@ def process_file(path, file_name):
     return result
 
 
-def process_folder(root, files):
-    current_path = pwd()
-    os.chdir(os.path.abspath(root))
-
-    folder_result = {}
-
-    for file_name in files:
-        file_result = process_file(root, file_name)
-        if file_result:
-            folder_result[file_name] = file_result
-
-    os.chdir(current_path)
-
-    return folder_result
-
-
 def find_slicot_function_names(r):
     function_names = {}
     for path, files in r.items():
@@ -105,9 +89,25 @@ class RecursiveFinder(object):
         for root, dirs, files in os.walk(self.abs_initial_path):
             # TODO : list of folders to ignore
             if '.git' not in root and '.idea' not in root and not root.startswith('..\\build'):
-                folder_list = process_folder(root, files)
+                folder_list = self.process_folder(root, files)
                 if folder_list:
                     self.result[root] = folder_list
+
+    @staticmethod
+    def process_folder(root, files):
+        current_path = pwd()
+        os.chdir(os.path.abspath(root))
+
+        folder_result = {}
+
+        for file_name in files:
+            file_result = process_file(root, file_name)
+            if file_result:
+                folder_result[file_name] = file_result
+
+        os.chdir(current_path)
+
+        return folder_result
 
 
 def main():
