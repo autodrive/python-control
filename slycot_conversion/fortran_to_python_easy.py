a6fa1c6ec28c9b0a00ef0ca8f0985b46e95dd30a
KangWon LEE <kangwon@gmail.com>
2016-07-01 12:19:45 +0900

    fortran_to_python_easy.py : added read file content

---
+++ slycot_conversion/fortran_to_python_easy.py
@@ -0,0 +1,26 @@
+"""
+convert fortran file to python if there is no go to
+"""
+import os
+
+
+def read_text_content(filename):
+    f = open(filename, 'rt')
+    txt = f.read()
+    f.close()
+
+    return txt
+
+
+def main(fortran_filename):
+    fortran_src = read_text_content(fortran_filename)
+    print(fortran_src)
+
+
+if __name__ == '__main__':
+    args = os.path.join('..', 'pyslicot', 'SB02MT.f')
+    from sys import argv
+
+    if 1 < len(argv):
+        args = argv[1]
+    main(args)
