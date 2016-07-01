b1c14136c8649e595d6895420eb1064a22e13e74
KangWon LEE <kangwon@gmail.com>
2016-07-01 12:50:27 +0900

    fortran_to_python_easy.py : convert comment lines

--- slycot_conversion/fortran_to_python_easy.py
+++ slycot_conversion/fortran_to_python_easy.py
@@ -12,13 +12,45 @@ def read_text_content(filename):
     return txt
 
 
+def is_comment(fortran_line):
+    whitespace = (' ', '\t')
+    result = not (fortran_line[0] in whitespace)
+    return result
+
+
+def fortran_filename_to_python_filename(fortran_filename):
+    return ''.join((os.path.splitext(fortran_filename)[0].lower(), '.py'))
+
+
+def write_python_file(python_filename, python_lines):
+    f = open(python_filename, 'wt')
+    for python_line in python_lines:
+        f.write('%s\n' % python_line)
+    f.close()
+
+
 def main(fortran_filename):
     fortran_src = read_text_content(fortran_filename)
-    print(fortran_src)
+
+    fortran_lines = fortran_src.splitlines()
+
+    python_lines = []
+
+    # line loop
+    for fortran_line in fortran_lines:
+        if is_comment(fortran_line):
+            python_line = '#' + fortran_line[1:]
+        else:
+            python_line = fortran_line
+        python_lines.append(python_line)
+
+    # write file
+    python_filename = fortran_filename_to_python_filename(fortran_filename)
+    write_python_file(python_filename, python_lines)
 
 
 if __name__ == '__main__':
-    args = os.path.join('..', 'pyslicot', 'SB02MT.f')
+    args = os.path.join(os.pardir, 'pyslicot', 'SB02MT.f')
     from sys import argv
 
     if 1 < len(argv):
