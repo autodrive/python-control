226532d8a65ac05d5864bef6feb68e397f33ba86
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-09-09 18:27:41 +0900

    added before install script

---
+++ .github/workflows/before_install.sh
@@ -0,0 +1,26 @@
+. set_os_env.sh
+# Install gfortran for testing slycot; use apt-get instead of conda in
+# order to include the proper CXXABI dependency (updated in GCC 4.9)
+if [[ "$SLYCOT" != "" ]]; then
+    sudo apt-get update -qq;
+    sudo apt-get install gfortran;
+fi
+
+# use miniconda to install numpy/scipy, to avoid lengthy build from source
+if [[ $RUNNER_OS ==  "macOS" ]]; then
+    . ./.github/workflows/wget_install_miniconda.sh
+fi
+
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda config --add channels python-control
+conda info -a
+conda create -q -n test-environment python="$PYTHON" pip coverage
+source activate test-environment
+# Install openblas if slycot is being used
+# also install scikit-build for the build process
+if [[ "$SLYCOT" != "" ]]; then
+    conda install openblas;
+    conda install -c conda-forge scikit-build;
+fi
